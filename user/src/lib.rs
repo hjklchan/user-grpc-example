3f24accd5e467065c879e6af51b60ef429274e90
Illusion <1161524405@qq.com>
2024-05-22 22:41:36 +0800

    create user store trait

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,1 +1,13 @@
 mod error;
+
+use abi;
+pub use error::UserError;
+
+#[async_trait]
+pub trait UserStore {
+    async fn create() -> Result<(), UserError>;
+    async fn update() -> Result<(), UserError>;
+    async fn delete() -> Result<(), UserError>;
+    async fn get() -> Result<(), UserError>;
+    async fn list() -> Result<(), UserError>;
+}
