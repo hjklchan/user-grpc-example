f02be3b3bdd25df9fa8509e77faeb965ad4b6652
Lucas chen <lucas.chen@gbo-asia.com>
2024-05-23 13:29:45 +0800

    Trait UserRepo is implemented

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -4,6 +4,11 @@ use abi;
 pub use error::UserError;
 
 use async_trait::async_trait;
+use sqlx::{Pool, mysql::MySql};
+
+pub struct DataStore {
+    pub pool: Pool<MySql>
+}
 
 #[async_trait]
 pub trait UserRepo {
@@ -12,4 +17,27 @@ pub trait UserRepo {
     async fn delete(&self, user_id: String) -> Result<(), UserError>;
     async fn get(&self, user_id: String) -> Result<abi::User, UserError>;
     async fn list() -> Result<Vec<abi::User>, UserError>;
+}
+
+#[async_trait]
+impl UserRepo for DataStore {
+    async fn create(&self, new_user: abi::User) -> Result<(), UserError> {
+        todo!()
+    }
+
+    async fn update(&self, user: abi::User) -> Result<(), UserError> {
+        todo!()
+    }
+
+    async fn delete(&self, user_id: String) -> Result<(), UserError> {
+        todo!()
+    }
+
+    async fn get(&self, user_id: String) -> Result<abi::User, UserError> {
+        todo!()
+    }
+
+    async fn list() -> Result<Vec<abi::User>, UserError> {
+        todo!()
+    }
 }
