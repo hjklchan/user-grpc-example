ca248a5fc6a7b406e02d300038fbf168cfe087fa
Lucas chen <lucas.chen@gbo-asia.com>
2024-05-23 13:19:47 +0800

    add UserRepo to manager the CRUD fns

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -3,11 +3,13 @@ mod error;
 use abi;
 pub use error::UserError;
 
+use async_trait::async_trait;
+
 #[async_trait]
-pub trait UserStore {
-    async fn create() -> Result<(), UserError>;
-    async fn update() -> Result<(), UserError>;
-    async fn delete() -> Result<(), UserError>;
-    async fn get() -> Result<(), UserError>;
-    async fn list() -> Result<(), UserError>;
+pub trait UserRepo {
+    async fn create(&self, new_user: abi::User) -> Result<(), UserError>;
+    async fn update(&self, user: abi::User) -> Result<(), UserError>;
+    async fn delete(&self, user_id: String) -> Result<(), UserError>;
+    async fn get(&self, user_id: String) -> Result<abi::User, UserError>;
+    async fn list() -> Result<Vec<abi::User>, UserError>;
 }
